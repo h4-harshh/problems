8e4d4f578ea780ae236ddcdaf14717f8fd6ff1b5
HARSH <harshnew2204@gmail.com>
2023-11-20 22:30:08 +0530

    Time: 167 ms (65.13%) | Memory: 102.4 MB (58.71%) - LeetSync

---
+++ 2471-minimum-amount-of-time-to-collect-garbage/minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        
+        int time=0;
+
+        int M_idx=0;
+        int P_idx=0;
+        int G_idx=0;
+
+        int total=0;
+
+        for(int i=0;i<garbage.size();i++)
+        {
+            int k=garbage[i].length();
+
+            for(int j=0;j<k;j++){
+
+                if(garbage[i][j]=='M'){
+                    M_idx=i;
+                }
+                if(garbage[i][j]=='P'){
+                    P_idx=i;
+                }
+                if(garbage[i][j]=='G'){
+                    G_idx=i;
+                }
+
+                total++;
+            } 
+        }
+        for(int i=1;i<travel.size();i++)
+        {
+            travel[i]=travel[i]+travel[i-1];
+        }
+        total+=M_idx>0?travel[M_idx-1]:0;
+        total+=P_idx>0?travel[P_idx-1]:0;
+        total+=G_idx>0?travel[G_idx-1]:0;
+
+        return total;
+    }
+};
