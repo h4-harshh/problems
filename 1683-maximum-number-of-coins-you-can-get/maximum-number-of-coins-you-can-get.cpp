f5b0bd5f56c70112f0bee471658382175c03d676
HARSH <harshnew2204@gmail.com>
2023-11-24 16:02:41 +0530

    Time: 86 ms (83.43%) | Memory: 53.6 MB (93.07%) - LeetSync

--- 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
+++ 1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int maxCoins(vector<int>& piles) {
+
         int n=piles.size();
 
-        int sum=0;
 
         sort(piles.begin(),piles.end());
 
-        int flag=n/3;
+        int b=0;
+        int m=n-2;
 
-        for(int i=n-2;i>=flag;i-=2){
-            cout<<piles[i]<<" ";
-            sum+=piles[i];
+        int sum=0;
+        while(b<m)
+        {
+            sum+=piles[m];
+            b++;
+            m-=2;
         }
-
         return sum;
     }
 };
