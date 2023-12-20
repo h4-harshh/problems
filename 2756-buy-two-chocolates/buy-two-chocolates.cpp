e871ff4d86ec5378e588a353917b4ac368d6587c
HARSH <harshnew2204@gmail.com>
2023-12-20 13:54:42 +0530

    Time: 27 ms (5.34%) | Memory: 46.9 MB (58.30%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -2,15 +2,38 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
         
-        sort(prices.begin(),prices.end());
+        // sort(prices.begin(),prices.end());
 
-        int sum=prices[0]+prices[1];
+        // int sum=prices[0]+prices[1];
 
-        if(sum<=money)
+        // if(sum<=money)
+        // {
+        //     return money-sum;
+        // }
+
+        // return money;
+
+
+        int minprice=INT_MAX;
+        int sec_min=INT_MAX;
+
+        for(auto price:prices)
         {
-            return money-sum;
+            if(price<minprice)
+            {
+                sec_min=minprice;
+                minprice=price;
+            }
+            else{
+                sec_min=min(sec_min,price);
+            }
         }
 
-        return money;
+        if(minprice+sec_min > money)
+        {
+            return money;
+        }
+
+        return money-(minprice+sec_min);
     }
 };
